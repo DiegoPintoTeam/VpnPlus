543196d8694a0f5915fb51d1e0b0cfe53341dc20
Diego Pinto (Team) <120763964+DiegoPintoTeam@users.noreply.github.com>
2024-10-08 23:30:53 -0500

    Add files via upload

--- VpnPlus/Modulos/proxy.py
+++ VpnPlus/Modulos/proxy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# VpnPlus By @By_mi_telegram
+# VpnPlus By @DiegoPintoTM
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
